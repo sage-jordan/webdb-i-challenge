fab37f6d6395668135aac63a3a7fb6d80856710a
sage-jordan <sagemjrodan@gmail.com>
2019-11-13 22:29:45 -0500

    queries.sql done, started on Router.  Yarn

---
+++ accountsRouter.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const db = require('./data/dbConfig.js');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    db('accounts').select('*')
+        .then(accounts => {
+            res.status(200).json(accounts);
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'Problem with database.', err });
+        })
+});
+
+router.get('/:id', (req, res) => {
+
+});
+
+router.post('/', (req, res) => {
+
+});
+
+router.put('/:id', (req, res) => {
+
+});
+
+router.delete('/:id', (req, res) => {
+
+});
+
+module.exports = router;
